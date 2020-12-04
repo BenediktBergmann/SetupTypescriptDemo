74bcb8f5ce3ad1c53991eb72e7b44c7e05982b0f
Benedikt Bergmann <github@bb-webpromotion.de>
2020-12-04 18:08:24 +0100

    Update helper.ts

--- ts/src/code/utils/helper.ts
+++ ts/src/code/utils/helper.ts
@@ -3,27 +3,14 @@ export async function loadRecords(
     query: string,
     maxPageSize?: number,
     errorCallback?: (error: any) => void,
-): Promise<any[] | null> {
-    return new Promise<any>((resolve, reject: any) => {
-        Xrm.WebApi.online.retrieveMultipleRecords(entityName, query, maxPageSize).then(
-            function success(result) {
-                if (
-                    result !== null &&
-                    result !== undefined &&
-                    result.entities !== null &&
-                    result.entities.length >= 1
-                ) {
-                    resolve(result.entities);
-                } else {
-                    resolve(null);
-                }
-            },
-            function (error) {
-                if (errorCallback !== null && errorCallback !== undefined) {
-                    errorCallback(error);
-                    reject();
-                }
-            },
-        );
-    });
+): Promise<any>{
+    try {
+        const result: any = await Xrm.WebApi.online.retrieveMultipleRecords(entityName, query, maxPageSize);
+
+        return result?.entities?.length >= 1 ? result.entities : null;
+    } catch (e: any) {
+        if (errorCallback != null) errorCallback(e);
+
+        throw Error();
+    }
 }
